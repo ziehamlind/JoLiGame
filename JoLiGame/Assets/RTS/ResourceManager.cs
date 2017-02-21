e2e2a65e2883f620dc1be4c9d54e154508eb5e7d
Your Name <you@example.com>
2017-02-21 13:07:43 +0100

    First Unity Ground + Camera + Light .Camera working (Mouse to sides and rotation)

---
+++ JoLiGame/Assets/RTS/ResourceManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RTS
+{
+    public static class ResourceManager
+    {
+        public static float ScrollSpeed { get { return 25; } }
+        public static float RotateSpeed { get { return 100; } }
+        public static float RotateAmount { get { return 10; } }
+        public static int ScrollWidth { get { return 15; } }
+
+        public static float MinCameraHeight { get { return 10; } }
+        public static float MaxCameraHeight { get { return 40; } }
+    }
+}
